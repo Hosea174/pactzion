13f0eb6854f2cc77ebd386f16edb8438f6da3577
Hosea174 <hossaena2018@gmail.com>
2024-01-04 12:23:36 +0300

    created responsive navbar

---
+++ src/scripts/main.js
@@ -0,0 +1,34 @@
+const navToggler = document.querySelector(".nav-toggler");
+const navMenu = document.querySelector(".nav-links");
+const navLinks = document.querySelectorAll(".navbar a");
+
+allEventListners();
+
+function allEventListners() {
+  navToggler.addEventListener("click", togglerClick);
+  navLinks.forEach((elem) => elem.addEventListener("click", navLinkClick));
+}
+
+function togglerClick() {
+  navToggler.classList.toggle("toggler-open");
+  navMenu.classList.toggle("open");
+}
+
+function navLinkClick() {
+  if (navMenu.classList.contains("open")) {
+    navToggler.click();
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  const currentPage = window.location.pathname.split("/").pop(); // Get the current page
+  console.log("currentPage: ");
+  console.log(currentPage);
+  const links = document.querySelectorAll("nav li a");
+  links.forEach((link) => {
+    const linkPage = link.getAttribute("data-page");
+    if (currentPage === linkPage) {
+      link.classList.add("active");
+    }
+  });
+});
