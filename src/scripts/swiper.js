a3fd6fc7448b4d2fff87fef9979b0b4810e67642
Hosea174 <hossaena2018@gmail.com>
2024-04-21 21:56:25 +0300

    made changan page responsive

--- src/scripts/swiper.js
+++ src/scripts/swiper.js
@@ -88,7 +88,7 @@ function changanAnimate() {
     gsap.to([modelHeaderActive, specsContainerActive], {
       duration: 1,
       opacity: 1,
-      y: 0,
+      y: -0.5,
       ease: "power2.out",
       filter: "blur(0px)",
     });
@@ -110,4 +110,5 @@ const colorVariants = new Swiper(".color-variants", {
   },
   // nested: true,
   slidesPerView: "auto",
+  grabCursor: true,
 });
