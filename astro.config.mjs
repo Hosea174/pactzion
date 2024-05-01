830e104231c53609359b3159c6dc177f9d2867c4
Hosea174 <hossaena2018@gmail.com>
2024-05-01 14:50:14 +0300

    removed astro compressor

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from 'astro/config';
 
-import compressor from "astro-compressor";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [compressor()]
 });
